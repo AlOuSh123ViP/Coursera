66cf3651ac24154ec39b61b2e93daa25ef07af46
Alaa warrak <47271041+AlOuSh123ViP@users.noreply.github.com>
2024-12-01 03:07:14 +0300

    Update weather_report.js

--- weather_report/weather_report.js
+++ weather_report/weather_report.js
@@ -2,7 +2,7 @@ function showweatherDetails(event) {
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = '32a7489772a6cbd6318971036d5cf80d'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = ''; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
     fetch(apiUrl)
